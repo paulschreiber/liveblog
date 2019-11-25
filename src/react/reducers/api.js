414b051a5d7bdd38e223c6e445cb68e125d27505
JayWood <jjwood2004@gmail.com>
2019-11-24 19:44:24 -0500

    Various updates, see notes
    Pass state.config to UPDATE_ENTRY_SUCCESS so we can delete entries if possible
    Pass a filter_view through services manager

--- src/react/reducers/api.js
+++ src/react/reducers/api.js
@@ -105,7 +105,12 @@ export const api = (state = initialState, action) => {
       const entries = { ...state.entries };
       const entry = { ...action.payload.entries[0] };
       const id = `id_${entry.id}`;
-      entries[id] = entry;
+
+      if (action.config.status !== entry.status && 'any' !== action.config.status) {
+        delete entries[id];
+      } else {
+        entries[id] = entry;
+      }
 
       return {
         ...state,
