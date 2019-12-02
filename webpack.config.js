d75e271fdfbc6cf687f786fe4e0850687b3930e8
Paul Schreiber <paul.schreiber@espn.com>
2019-12-02 17:41:22 -0500

    add social js to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ const webpackConfig = {
   entry: {
     app: path.join(__dirname, paths.entry),
     amp: path.join(__dirname, './src/react/amp.js'),
+    social: path.join(__dirname, './src/js/social.js'),
     theme: path.join(__dirname, './src/react/theme.js'),
     dashboard: path.join(__dirname, './src/react/dashboard.js'),
   },
