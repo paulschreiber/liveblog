ae1b85dd077845a1207b346b420b6695edcda05d
Paul Schreiber <paul.schreiber@espn.com>
2020-03-04 16:03:41 -0500

    deref object

--- classes/class-liveblog-entry.php
+++ classes/class-liveblog-entry.php
@@ -417,7 +417,7 @@ class Liveblog_Entry {
 		$timestamp    = current_time( 'timestamp' );
 		$content      = Liveblog_Webhook_API::sanitize_entry( $entry_data->event->text, $liveblog_id, $entry_data->event->files ?? [] );
 		$author_names = self::get_authors( $author_id );
-		$author_name  = is_array( $author_names, $author_names[0] ) && $author_names ? $author_names[0] : '';
+		$author_name  = is_array( $author_names, $author_names[0] ) && $author_names ? $author_names[0]->name : '';
 
 		return "[liveblog_entry author_id='{$author_id}' timestamp='{$timestamp}' author_name='{$author_name}']
 		{$content['content']}
