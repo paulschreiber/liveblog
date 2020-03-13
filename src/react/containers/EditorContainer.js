b8b7c9503ed6cbf720f50fbc77b1cd8482c0cd71
PypWalters <stephanie.walters@10up.com>
2020-03-13 16:10:12 -0400

    move close to the editor component

--- src/react/containers/EditorContainer.js
+++ src/react/containers/EditorContainer.js
@@ -93,6 +93,22 @@ class EditorContainer extends Component {
     });
 
     this.getUsers = debounce(this.getUsers.bind(this), props.config.author_list_debounce_time);
+
+    this.handleWindowClose = (e) => {
+      console.log('1');
+      if (props.isEditing) {
+        console.log('2');
+        this.close(e);
+      }
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.handleWindowClose);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.handleWindowClose);
   }
 
   setReadOnly(state) {
