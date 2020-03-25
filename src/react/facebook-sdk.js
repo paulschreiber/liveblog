510a69f5fb3fd4e4a257b0ee229e686b6c2b56f2
Paul Schreiber <paul.schreiber@espn.com>
2020-03-25 11:20:31 -0400

    fix JS linter errors

--- src/react/facebook-sdk.js
+++ src/react/facebook-sdk.js
@@ -1,18 +1,18 @@
-if ( ! document.getElementById('fb-root')) {
-
-	// create div required for fb
-	const fbDiv = document.createElement('div');
-	fbDiv.id = 'fb-root';
-	document.body.appendChild(fbDiv);
+if (!document.getElementById('fb-root')) {
+  // create div required for fb
+  const fbDiv = document.createElement('div');
+  fbDiv.id = 'fb-root';
+  document.body.appendChild(fbDiv);
 }
 
 // inject fb sdk.js
-document.addEventListener( 'DOMContentLoaded', function() {
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) {return;}
-		js = d.createElement(s); js.id = id;
-		js.src = "https://connect.facebook.net/en_US/sdk.js";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-} )
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.getElementById('facebook-jssdk')) {
+    return;
+  }
+  const fjs = document.getElementsByTagName('script')[0];
+  const js = document.createElement('script');
+  js.id = 'facebook-jssdk';
+  js.src = 'https://connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+});
