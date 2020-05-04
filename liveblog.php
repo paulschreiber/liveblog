82a11415d94ecc769dfaf2655a78bebc32928f2d
Paul Schreiber <paulschreiber@gmail.com>
2020-05-04 11:29:46 -0400

    bump version number to 1.98; update author list

--- liveblog.php
+++ liveblog.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: Liveblog
- * Plugin URI: http://wordpress.org/extend/plugins/liveblog/
+ * Plugin URI:  http://wordpress.org/extend/plugins/liveblog/
  * Description: Empowers website owners to provide rich and engaging live event coverage to a large, distributed audience.
- * Version:     1.9.7
- * Author:      WordPress.com VIP, Big Bite Creative and contributors
- * Author URI: https://github.com/Automattic/liveblog/graphs/contributors
+ * Version:     1.9.8
+ * Author:      10up, Paul Schreiber, WordPress.com VIP, Big Bite Creative and contributors
+ * Author URI:  https://github.com/paulschreiber/liveblog/graphs/contributors
  * Text Domain: liveblog
  */
 
